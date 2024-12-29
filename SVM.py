afcccb903fafc691aea59549e58127dfe6c262b0
Mihail <miha27022005@icloud.com>
2024-12-29 20:55:07 +0000

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -5,20 +5,14 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
 
-# Создание датасета для бинарной классификации
-X, y = make_classification(n_samples=1000,  # Количество образцов
-                           n_features=20,  # Количество признаков
-                           n_classes=2,  # Два класса
-                           n_informative=2,  # Количество информативных признаков
-                           n_redundant=2,  # Количество избыточных признаков
-                           random_state=42)  # Фиксируем random_state для воспроизводимости
-
+X, y = make_classification(n_samples=1000, n_features=20, n_classes=2, n_informative=2, n_redundant=2, random_state=42)
 X = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(20)])
 y = pd.Series(y, name='target')
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 class SVM:
     def __init__(self, learning_rate=0.001, n_iter=1000, coef_regularization=0.01):
@@ -31,21 +25,30 @@ class SVM:
         n_samples, n_features = X.shape
         y = np.where(y <= 0, -1, 1)
         self.weights_linear_combination = np.zeros(n_features)
-
         for _ in range(self.n_iter):
             for indx, x_i in enumerate(X):
                 linear_comb = np.dot(x_i, self.weights_linear_combination)
                 margin = y[indx] * linear_comb
-
                 if margin < 1:
                     gradient = -y[indx] * x_i + 2 * self.coef_regularization * self.weights_linear_combination
+                    self.weights_linear_combination -= self.learning_rate * gradient
                 else:
                     gradient = 2 * self.coef_regularization * self.weights_linear_combination
+                    self.weights_linear_combination -= self.learning_rate * gradient
 
-# Предсказания и оценка точности
+    def predict(self, X):
+        linear_comb = np.dot(X, self.weights_linear_combination)
+        return np.sign(linear_comb)
+
+model = SVM(learning_rate=0.001, n_iter=1000, coef_regularization=0.01)
+model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
+
+from sklearn.svm import SVC
+model2 = SVC(kernel='linear')
+model2.fit(X_train, y_train)
 predictionsSK = model2.predict(X_test)
 accuracySK = accuracy_score(y_test, predictionsSK)
 
-print(f'Accuracy: {accuracy}, \n {'-'* 400} \n AccuracySK: {accuracySK}')
+print(f'Accuracy: {accuracy}, \n {"-"* 400} \n AccuracySK: {accuracySK}')
